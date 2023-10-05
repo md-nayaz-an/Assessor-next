b36a5ab8b4cae9126c6af50eacc1899a62c17f56
Mohammed Nayaz <mohammednayazan@gmail.com>
2023-10-05 13:08:51 +0530

    metadata removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,8 @@
+"use client";
+
 import Nav from '@components/Nav';
 import '@styles/globals.css';
 import { RecoilRoot } from 'recoil';
-
-export const metadata = {
-  title: 'Assessor App',
-  description: 'Assessing the future Entrepreneurs for their decision making skills',
-}
 
 export default function RootLayout({ children }) {
   	return (
