ef72ce72c45dbaeee02124dadb2d3c7790238c2a
Mohammed Nayaz <mohammednayazan@gmail.com>
2024-02-04 21:43:16 +0530

    navbar z fix

--- src/components/ClientNav.js
+++ src/components/ClientNav.js
@@ -30,7 +30,7 @@ const ClientNav = () => {
     ]
 
     return (
-        <nav className="flex-between w-full pt-3 drop-shadow-lg rounded">
+        <nav className="flex-between w-full pt-3 drop-shadow-lg rounded z-50">
             {/*<Link href="/" className="flex gap-2 flex-center">
                 <Image
                     src="/assets/icons/a.svg"
@@ -66,7 +66,7 @@ const ClientNav = () => {
                                 //<Link href="/api/auth/signout?callbackUrl=/">
                                   //  Logout
                                 //</Link> 
-                                <div className="dropdown dropdown-bottom">
+                                <div className="dropdown dropdown-bottom z-100">
                                     <div tabIndex={0} role="button" className="avatar">
                                         <div className="w-8 rounded-full">
                                             <Image 
@@ -112,7 +112,7 @@ const ClientNav = () => {
                         <label tabIndex={0} className="btn btn-ghost">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                             {
                                 menu.map((item, key) => (
                                     <li key={key}>
